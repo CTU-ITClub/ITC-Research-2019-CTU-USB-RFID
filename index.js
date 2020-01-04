2aaa4117d6186becaf4f8955c685e4f6c60621df
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-04 21:19:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/chat", chat);
 api.activate(app);
