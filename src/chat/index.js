2872e2634b288e92df21a4072c24faf15365b723
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-04 22:30:05 +0700

    Update index.js

--- src/chat/index.js
+++ src/chat/index.js
@@ -16,7 +16,7 @@ function send(id, text) {
     qs: {
       /** Access code token, get it at https://developers.facebook.com/apps/504492863527014/messenger/settings/*/
       access_token:
-        "EAAHK1WXsGGYBAFS6ZCcYFv0nlZCb9QZCxCxtKlnuKoN2v0SxNrcqF4SCJDdbb73ddMTnpgn477Q3OqZAsa5PCZAgWSRVKyhron1afqpZBPsZBTwZAp3ZBgWGlydJfmUXVTZCEMNoVzigZADGiCZAiZAm5XGQdR9q6kp6nzzIZBgJbSZCoVX2EYZCdeYz945fQOZBS4h6uUlcZD"
+        "EAAHK1WXsGGYBAKwtrixva6KzgZBUKKvKAZB3S7ZCOZBCGFvaJxahkZBDvh2RWzzGrF8Jy8YyT4cZBRpV13SdtNkU7mdZBFrdAeNJBZCgko0Em1icTZAxWys6XG45nCS913q2H4BU0QSB9PZA2Ywvz9NgibZCBUV1fPwt773QrhXLlCgyoq02pDlcOUSPIpsUsF7rCoZD"
     },
     method: "POST",
     /** Message send in JSON type */
@@ -33,10 +33,11 @@ function send(id, text) {
 
 // Authenticate
 router.get("/", (req, res) => {
-  if (req.query["hub.verify_token"] === "daomtthuan") {
-    res.send(req.query["hub.challenge"]);
+  if (req.query["hub.verify_token"] == "daomtthuan") {
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    res.status(200).send("Error, wrong validation token");
   }
-  res.send("Error, wrong validation token");
 });
 
 // Receive and analyze
