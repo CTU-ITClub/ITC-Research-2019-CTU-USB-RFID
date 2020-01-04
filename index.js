e062d5609c5c5431cac1611bddec3f446fdb25d6
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-04 09:50:17 +0700

    rename

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const api = require("./src/api");
-const webhook = require("./src/chat/webhook");
+const chat = require("./src/chat");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/public")));
-app.use("/api/webhook", webhook);
+app.use("/chat", chat);
 api.activate(app);
 app.listen(port, () => console.log(`RFID Checkin Server is listening on http://127.0.0.1:${port}/`));
