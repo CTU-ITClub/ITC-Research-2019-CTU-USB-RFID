c79b3196030e5a86fdd0652bd12ec4f9f4b958c5
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-04 21:39:39 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- * @author Daomtthuan
- * @email dao.mt.thuan@gmail.com
- * @create date 2020-01-03 17:29:58
- * @modify date 2020-01-04 09:41:02
- * @desc Nodejs server
- */
-
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
