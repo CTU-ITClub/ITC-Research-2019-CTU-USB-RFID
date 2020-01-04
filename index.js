e4214dc72711e9c348666966cdd2cff5411cf385
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-04 21:47:02 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const api = require("./src/api");
 const chat = require("./src/chat");
@@ -9,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static("public"));
 app.use("/chat", chat);
 api.activate(app);
 app.listen(port, () => console.log(`RFID Checkin Server is listening on http://127.0.0.1:${port}/`));
