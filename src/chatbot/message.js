2dd55699b038e24c8e5784dca14b3905ea4973c2
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-05 16:24:36 +0700

    Update message.js

--- src/chatbot/message.js
+++ src/chatbot/message.js
@@ -1,9 +1,13 @@
-const hello = `Hello, I'm RFID Checkin chatbox. I'm from IT Club Can Tho University.`;
+const hello = `Hello, I'm RFID Checkin chatbox.
+I'm from IT Club Can Tho University.`;
+
 const help = `Usage:
-  hello, hi  -  Say hello
-  help  -  Display help
-  chat  -  Send message to admin`;
-const unknow = `Sorry, I can't understand. Please type "help" to display help`;
+    hello, hi  -  Say hello
+    help  -  Display help
+    chat  -  Send message to admin`;
+
+const unknow = `Sorry, I can't understand.
+Please type "help" to display help`;
 
 module.exports = {
   hello,
