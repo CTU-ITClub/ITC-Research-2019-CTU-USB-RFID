7bc0d3463a225237c427d28f96bbe6bb638ba7d1
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2020-01-07 03:02:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const os = require("os");
 const api = require("./src/api");
 const chatbot = require("./src/chatbot");
 
